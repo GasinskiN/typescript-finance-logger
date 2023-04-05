abefc61fc5a888245a6c99b9288811681f4fe4fe
GasinskiN <gasinskinorman@gmail.com>
2023-04-05 20:20:15 +0200

    Make source folder

---
+++ src/sandbox.ts
@@ -0,0 +1,21 @@
+let character = "wario";
+let age = 30;
+let isBlackBelt = false;
+
+
+character = "luigi";
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+}
+console.log(circ(10))
+
+let ninjaOne: object;
+
+ninjaOne = {name: "yoshi", age: 33};
+console.log(ninjaOne);
+ninjaOne = {height: 180};
+console.log(ninjaOne);
+
+let ale: any=25;
+
