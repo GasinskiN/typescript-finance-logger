8dd11fff82b3e35376602774bbb2959ac43c9cb9
GasinskiN <gasinskinorman@gmail.com>
2023-04-07 15:26:59 +0200

    Add a class for the invoice and push to an array of invoices once the submit button is called

--- public/app.js
+++ public/app.js
@@ -1,10 +1,26 @@
 "use strict";
+class Invoice {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}.`;
+    }
+}
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const invoices = [];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    invoices.push(new Invoice(toFrom.value, details.value, amount.valueAsNumber));
+    toFrom.value = "";
+    details.value = "";
+    amount.value = "";
+    invoices.forEach(inv => { console.log(inv.format()); });
 });
+console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+class Invoice {
+    readonly client: string;
+    private details: string;
+    amount: number;
+
+    constructor(a: string, b: string, c: number){
+        this.client = a;
+        this.details = b;
+        this.amount = c;
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}.`
+    }
+}
+
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 
@@ -6,10 +23,14 @@ const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+const invoices: Invoice[] = [];
 
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    
-
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    invoices.push(new Invoice(toFrom.value, details.value, amount.valueAsNumber));
+    toFrom.value = "";
+    details.value = "";
+    amount.value = "";
+    invoices.forEach(inv => {console.log(inv.format())});
 })
+console.log(invoices);
