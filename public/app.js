65d5663a9e3bda17997a6e99bf4944c74c9c1307
GasinskiN <gasinskinorman@gmail.com>
2023-04-08 19:05:56 +0200

    Add sorting to the list

--- public/app.js
+++ public/app.js
@@ -5,15 +5,23 @@ const type = document.querySelector("#type");
 const toFrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const sort = document.querySelector("#sort");
 const dataForRendering = [];
 let ul = document.querySelector(".item-list");
 const list = new ListElements(ul);
+sort.addEventListener("change", (e) => {
+    if (sort.value === "newest") {
+        list.render(dataForRendering, "newest");
+    }
+    else {
+        list.render(dataForRendering, "oldest");
+    }
+});
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     dataForRendering.push(new Invoice(type.value, toFrom.value, details.value, amount.valueAsNumber));
     toFrom.value = "";
     details.value = "";
     amount.value = "";
-    ul.innerHTML = "";
-    list.render(dataForRendering, "newest");
+    list.render(dataForRendering, sort.value);
 });
--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,32 @@ import Invoice from "./classes/Invoice.js";
 import ListElements from "./classes/ListElements.js";
 import HasFormatter from "./interfaces/HasFormatter.js";
 
-
-
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type") as HTMLSelectElement;
 const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+const sort = document.querySelector("#sort") as HTMLSelectElement;
 
 const dataForRendering: HasFormatter[] = [];
 
 let ul = document.querySelector(".item-list") as HTMLUListElement;
 const list = new ListElements(ul);
 
+sort.addEventListener("change", (e: Event) =>{
+    if (sort.value === "newest"){
+        list.render(dataForRendering, "newest");
+    } else {
+        list.render(dataForRendering, "oldest");
+    }
+});
 
 
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    dataForRendering.push(new Invoice(type.value as "payment"|"invoice", toFrom.value, details.value, amount.valueAsNumber));
+    dataForRendering.push(new Invoice(type.value, toFrom.value, details.value, amount.valueAsNumber));
     toFrom.value = "";
     details.value = "";
     amount.value = "";
-    ul.innerHTML = "";
-    list.render(dataForRendering, "newest");
-})
+    list.render(dataForRendering, sort.value);
+});
