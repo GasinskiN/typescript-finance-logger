d9a290f60d8292dde16700e49a1ae797cf698c06
GasinskiN <gasinskinorman@gmail.com>
2023-04-08 17:28:12 +0200

    Make different lists for invoice and payment

--- public/app.js
+++ public/app.js
@@ -1,16 +1,23 @@
 import Invoice from "./classes/Invoice.js";
+import Payment from "./classes/Payment.js";
 const form = document.querySelector(".new-item-form");
 const type = document.querySelector("#type");
 const toFrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 const invoices = [];
+const payments = [];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    invoices.push(new Invoice(toFrom.value, details.value, amount.valueAsNumber));
+    if (type.value === "invoice") {
+        invoices.push(new Invoice(toFrom.value, details.value, amount.valueAsNumber));
+    }
+    else if (type.value === "payment") {
+        payments.push(new Payment(toFrom.value, details.value, amount.valueAsNumber));
+    }
     toFrom.value = "";
     details.value = "";
     amount.value = "";
     invoices.forEach(inv => { console.log(inv.format()); });
+    payments.forEach(pay => { console.log(pay.format()); });
 });
-console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Invoice from "./classes/Invoice.js";
+import Payment from "./classes/Payment.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type") as HTMLSelectElement;
@@ -7,13 +8,18 @@ const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
 const invoices: Invoice[] = [];
+const payments: Payment[] = [];
 
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
-    invoices.push(new Invoice(toFrom.value, details.value, amount.valueAsNumber));
+    if (type.value === "invoice"){
+        invoices.push(new Invoice(toFrom.value, details.value, amount.valueAsNumber));
+    } else if (type.value === "payment"){
+        payments.push(new Payment(toFrom.value, details.value, amount.valueAsNumber))
+    }
     toFrom.value = "";
     details.value = "";
     amount.value = "";
     invoices.forEach(inv => {console.log(inv.format())});
+    payments.forEach(pay => {console.log(pay.format())});
 })
-console.log(invoices);
