beef01a925ee38af9b2752f5e8558d68fa7bb091
GasinskiN <gasinskinorman@gmail.com>
2023-04-09 22:37:03 +0200

    Remove unnecessarily restrictive type for Invoice

--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -3,7 +3,7 @@ import HasFormatter from "../interfaces/HasFormatter"
 
 export default class Invoice implements HasFormatter {
     constructor(
-        readonly type: "invoice" | "payment",
+        readonly type: string,
         readonly client: string,
         readonly details: string,
         readonly amount: number
